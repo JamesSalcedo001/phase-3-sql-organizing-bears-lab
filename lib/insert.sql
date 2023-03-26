682919bb0b4ad4f1cbf5bbb43ac0ce54344b2c0a
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-03-26 15:43:46 -0500

    commit tree c8ceead8a08ae0f64778910bcb1b93abfa4b8259

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "brown", "grouchy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 3, "M", "black", "crazy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "brown", "relaxed", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "brown", "lazy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "black", "angry", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 1, "F", "brown", "sad", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 3, "F", "black", "happy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "brown", "sweet", true);
